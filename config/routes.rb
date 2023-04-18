79615c237fa84751e9bdc41086569217260d98fb
Jufri Ali <faqat0_0@hotmail.com>
2023-04-18 23:40:37 +0800

    Created home & about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'about' => 'pages#about'
+  get 'contact_us' => 'pages#contact_us'
 end
