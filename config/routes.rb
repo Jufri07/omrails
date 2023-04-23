21838314dbd13d1887223ff1f1f3649f1a471fe2
Jufri Ali <faqat0_0@hotmail.com>
2023-04-24 00:30:05 +0800

    created sign in, sign up, edited routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+    get '/signin', to: 'devise/sessions#new'
+    get '/signout', to: 'devise/sessions#destroy'
+    # delete "signout" => 'devise/sessions#destroy'
+    get '/signup', to: 'devise/registrations#new'
+  end
+
+
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
