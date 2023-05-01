3d5d7d89704754bc20a0ba3fe85d6b526ab45077
Jufri Ali <faqat0_0@hotmail.com>
2023-05-01 21:29:40 +0800

    added permit params for active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -14,5 +14,17 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
+  # this permit_params is for admin to change email, admin. (if need to change eg name, can just add in :name)
+  permit_params :email, :admin
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :email
+      f.input :admin
+      # but here also must put in f.input :name if want this name to be edited by admin
+    end
+    f.actions
+  end
 end
