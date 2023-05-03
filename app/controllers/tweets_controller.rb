3d047b207d8bf5800f3ead0ac76027acab43231c
Jufri Ali <faqat0_0@hotmail.com>
2023-05-03 22:17:36 +0800

    tweets scaffold created

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,56 @@
+class TweetsController < ApplicationController
+
+  # GET /tweets or /tweets.json
+  def index
+    @tweets = Tweet.all
+  end
+
+  # GET /tweets/1 or /tweets/1.json
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  # GET /tweets/new
+  def new
+    @tweet = Tweet.new
+  end
+
+  # GET /tweets/1/edit
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  # POST /tweets or /tweets.json
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweet_url(@tweet), notice: "Tweet was successfully created."
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /tweets/1 or /tweets/1.json
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweet_url(@tweet), notice: "Tweet was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /tweets/1 or /tweets/1.json
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_url, notice: "Tweet was successfully destroyed."
+  end
+
+  private
+
+    # Only allow a list of trusted parameters through.
+    def tweet_params
+      params.require(:tweet).permit(:user_id, :content)
+    end
+end
